60b6392355c556f93ff3888b76b22dab8e99f663
Svetlana Levinsohn <svetlana.levinsohn@gmail.com>
2022-05-21 12:00:12 -0700

    Lesson 6

--- features/steps/amazon_main_page_steps.py
+++ features/steps/amazon_main_page_steps.py
@@ -1,10 +1,14 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from behave import given, when, then, step
+from time import sleep
 
 
 SEARCH_INPUT = (By.ID, 'twotabsearchtextbox')
 SEARCH_BTN = (By.ID, 'nav-search-submit-button')
+SEARCH_RESULTS = (By.CSS_SELECTOR, "[data-component-type='s-search-result']")
+PRODUCT_TITLE = (By.CSS_SELECTOR, 'h2 span.a-text-normal')
+PRODUCT_IMG = (By.CSS_SELECTOR, ".s-image[data-image-latency='s-product-image']")
 HAM_MENU_BTN = (By.ID, 'nav-hamburger-menu')
 FOOTER_LINKS = (By.CSS_SELECTOR, 'td.navFooterDescItem a')
 SIGN_IN_BTN = (By.CSS_SELECTOR, "#nav-signin-tooltip .nav-action-inner")
@@ -29,15 +33,39 @@ def click_sign_in_btn(context):
     sign_in_btn.click()
 
 
+@when('Wait for {seconds} seconds')
+def wait_sec(context, seconds):
+    sleep(int(seconds)) # "5" => 5
+
+
 @then('Verify hamburger menu btn present')
 def verify_ham_menu(context):
-    context.driver.find_element(*HAM_MENU_BTN)
+    ham_menu = context.driver.find_element(*HAM_MENU_BTN)
+    ham_menu.click()
 
 
 @then('Verify there are {expected_amount} footer links')
 def verify_footer_links_count(context, expected_amount):
     expected_amount = int(expected_amount)  # '38' => 38
-
     footer_links = context.driver.find_elements(*FOOTER_LINKS)  # [Webelement1, Webelement2, ..]
-
     assert len(footer_links) == expected_amount, f'Expected {expected_amount} links, but got {len(footer_links)}'
+
+
+@then('Verify that every product has a name and an image')
+def verify_products_name_img(context):
+    all_products = context.driver.find_elements(*SEARCH_RESULTS)
+    for product in all_products:
+        title = product.find_element(*PRODUCT_TITLE).text
+        assert title, 'Error! Title should not be blank'
+        product.find_element(*PRODUCT_IMG)
+
+
+@then('SignIn popup is present')
+def verify_signin_popup_present(context):
+    context.driver.wait.until(EC.element_to_be_clickable(SIGN_IN_BTN), 'Sign in btn not clickable')
+
+
+@then('SignIn popup disappears')
+def verify_signin_popup_not_present(context):
+    context.driver.wait.until_not(EC.element_to_be_clickable(SIGN_IN_BTN), 'Sign in btn did not disappear')
+
--- features/tests/amazon_search.feature
+++ features/tests/amazon_search.feature
@@ -19,3 +19,8 @@ Feature: Tests for Amazon search
   Scenario: User sees correct amount of footer links
     Given Open Amazon page
     Then Verify there are 38 footer links
+
+  Scenario: Verify that user can see product names and images
+    Given Open Amazon page
+    When Search for coffee
+    Then Verify that every product has a name and an image
